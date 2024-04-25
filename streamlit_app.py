e8ef4260139fdf55c2c9a7403271182b66feb199
Tim McCarthy <tkmccarthy@gmail.com>
2024-04-25 10:30:47 -0500

    Update streamlit_app.py
    more display changes for nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-# API call to get nutrition information
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -45,6 +41,11 @@ if ingredients_list:
 
     # st.write(my_insert_stmt)
     # st.stop()
+
+    # API call to get nutrition information
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    # st.text(fruityvice_response.json())
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     time_to_insert = st.button('Submit Order')
     
