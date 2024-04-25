3d7e96bf22c541e4c4343beb71b15400a5357bd1
Tim McCarthy <tkmccarthy@gmail.com>
2024-04-25 10:33:50 -0500

    Update streamlit_app.py
    moved nutri info to if statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,14 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-
-
 if ingredients_list:
 
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     # st.write(ingredients_string)
 
@@ -41,12 +41,7 @@ if ingredients_list:
 
     # st.write(my_insert_stmt)
     # st.stop()
-
-    # API call to get nutrition information
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    # st.text(fruityvice_response.json())
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-    
+  
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
